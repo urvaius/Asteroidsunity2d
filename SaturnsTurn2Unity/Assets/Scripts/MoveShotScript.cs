cc6f22976465c7b5a3dc449d8ed422810c375612
Dave Arneson <urvaius@gmail.com>
2015-02-12 13:40:52 -0800

    first commint base of saturnsturn2

---
+++ SaturnsTurn2Unity/Assets/Scripts/MoveShotScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveShotScript : MonoBehaviour
+{
+    
+    public Vector2 speed = new Vector2(10, 10);
+
+    //direction
+    public Vector2 direction = new Vector2(-1, -1);
+
+
+	// Use this for initialization
+	void Start ()
+    
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    
+    {
+        // movement
+        Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+        //movement.Normalize();
+        movement *= Time.deltaTime;
+        transform.TransformPoint(Vector3.forward * 10);
+
+
+        
+       // transform.Rotate(movement, 20);
+       // transform.rigidbody2D.AddForceAtPosition(speed, direction);
+        transform.Translate(movement);
+        
+	}
+}
---
+++ SaturnsTurn2Unity/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Launch projectile
+/// </summary>
+public class WeaponScript : MonoBehaviour
+{
+    //--------------------------------
+    // 1 - Designer variables
+    //--------------------------------
+
+    /// <summary>
+    /// Projectile prefab for shooting
+    /// </summary>
+    public Transform shotPrefab;
+
+    /// <summary>
+    /// Cooldown in seconds between two shots
+    /// </summary>
+    public float shootingRate = 0.25f;
+    public float speed = 10;
+    //--------------------------------
+    // 2 - Cooldown
+    //--------------------------------
+
+    private float shootCooldown;
+
+    void Start()
+    {
+        shootCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    //--------------------------------
+    // 3 - Shooting from another script
+    //--------------------------------
+
+    /// <summary>
+    /// Create a new projectile if possible
+    /// </summary>
+    public void Attack(bool isEnemy)
+    {
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+
+            // Create a new shot
+           // var shotTransform = Instantiate(shotPrefab) as Transform;
+            Vector3 shotPosition = new Vector3(transform.position.x,transform.position.y );
+             var shotTransform = Instantiate(shotPrefab,shotPosition,Quaternion.Euler(shotPosition)) as Transform;
+           
+            
+            // Assign position
+           //  Vector3 newRotation = transform.rotation.eulerAngles;
+           //  newRotation.x += transform.position.x;
+           //  newRotation.y += transform.position.y;
+           //  shotTransform.rotation = Quaternion.Euler(newRotation);
+           //// shotTransform.position = transform.position;
+            
+           // shotTransform.rigidbody.AddRelativeForce(Vector3.forward * speed);
+
+            // The is enemy property
+            ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+            if (shot != null)
+            {
+                shot.isEnemyShot = isEnemy;
+            }
+
+            // Make the weapon shot always towards it
+            MoveShotScript move = shotTransform.gameObject.GetComponent<MoveShotScript>();
+            if (move != null)
+            {
+                
+                move.direction = this.transform.up; // towards in 2D space is the right of the sprite
+               
+            }
+        }
+    }
+
+    /// <summary>
+    /// Is the weapon ready to create a new projectile?
+    /// </summary>
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+}
